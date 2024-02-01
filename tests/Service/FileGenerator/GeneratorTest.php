da7fa07e2685b9409585f18c679d07a0d7947c6b
Leszek Koziatek <leszek.koziatek@dzielo.pl>
2024-02-01 19:38:17 +0100

    Add PHPUnit

---
+++ tests/Service/FileGenerator/GeneratorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service\FileGenerator;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneratorTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
